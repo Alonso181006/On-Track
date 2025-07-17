6d482d87d79da8af7de665617c8c7a8b8ab3e45a
andrwhli <andrwhli@students.cs.ubc.ca>
2025-07-17 00:21:29 -0600

    Templated all tabs, fixed import/export of current assets

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,12 +1,37 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Tabs } from 'expo-router'
+
+import { icons } from "../../constants"
 
 const Tabslayout = () => {
-  return (
-    <View>
-      <Text>Tabslayout</Text>
-    </View>
-  )
+    return (
+        <>
+            <Tabs>
+                <Tabs.Screen
+                    name='home'
+                    options={{
+                        title: "Home",
+                        headerShown: false,
+                    }}
+                />
+                <Tabs.Screen
+                    name='create'
+                    options={{
+                        title: "Create",
+                        headerShown: false,
+                    }}
+                />
+                <Tabs.Screen
+                    name='profile'
+                    options={{
+                        title: "Profile",
+                        headerShown: false,
+                    }}
+                />
+            </Tabs>
+        </>
+    )
 }
 
 export default Tabslayout
---
+++ constants/icons.js
@@ -0,0 +1,9 @@
+import home from "../assets/icons/home.png"
+import create from "../assets/icons/plus.png"
+import profile from "../assets/icons/user.png"
+
+export default {
+    home,
+    create,
+    profile
+}
