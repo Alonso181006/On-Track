26384e88cf265699da7e814b7069eb6426ed9ca5
andrwhli <andrwhli@students.cs.ubc.ca>
2025-07-17 00:12:13 -0600

    Imported (tabs) icons, temporary link to view (tabs), safeareaview for all pages

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { Text, View } from "react-native";
+import { Link } from 'expo-router';
 
-export default function Index() {
+export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-rose-500">
       <Text className="text-white">Hello Andrew</Text>
+      <Link href='/home' style={{ color: 'blue' }}>Start now!</Link>
     </View>
   );
 }
