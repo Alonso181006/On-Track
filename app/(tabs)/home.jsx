1b13cc3bb2f22bfdbf86f88ff6741f2ade397a51
andrwhli <andrwhli@students.cs.ubc.ca>
2025-07-20 03:25:28 -0600

    Templated nextup habit feature for home screen, removed unnecessary imports

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,13 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+
+// lots to fix here, lots of hardcoded values, and static values that should be dynamic
 
 const Home = () => {
-  return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
-  )
+    return (
+        <View className="w-[330px] h-[500px] mt-[150px] mx-auto">
+            <View className="flex-row justify-between">
+                <Text className="ml-2 mt-2">Nextup</Text>
+                <Text className="mr-2 mt-2">Time</Text>
+            </View>
+            <View className="justify-center items-center w-[330px] h-[100px] rounded-[12] bg-[#EFEBEB]">
+                <TouchableOpacity className="w-[316px] h-[75px] rounded-[12] bg-[#D9D9D9]">
+                    <View className="flex-row my-auto justify-between">
+                        <Text className="ml-2">Habit</Text>
+                        <Text className="mr-2">Duration</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 export default Home 
