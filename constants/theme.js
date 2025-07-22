1d05ac7bf5f044696594b3755171a44649762eb3
andrwhli <andrwhli@students.cs.ubc.ca>
2025-07-22 01:59:31 -0600

    Created constants for colors and fontfamily

---
+++ constants/theme.js
@@ -0,0 +1,14 @@
+const COLORS = {
+    primary: "#D9D9D9",
+    secondary: "#EFEBEB",
+}
+
+const FONTFAMILY = {
+    jaro9: "Jaro_9pt-Regular",
+    jaro24: "Jaro_24pt-Regular",
+    jaro36: "Jaro_36pt-Regular",
+    jaro60: "Jaro_60pt-Regular",
+    jaro: "Jaro-Regular",
+}
+
+export { COLORS, FONTFAMILY }
