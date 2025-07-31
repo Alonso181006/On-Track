5a8d7b1d075ff956aa38b9778eab23224cdd8cba
andrwhli <andrwhli@students.cs.ubc.ca>
2025-07-30 20:15:43 -0600

    Created custom habit component, universal for both nextup display as well as regular display

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -2,10 +2,20 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import { React, useState } from 'react'
 import { Calendar } from 'react-native-calendars'
 
+import Habit from '../../components/Habit'
+
 // lots to fix here, lots of hardcoded values, and static values that should be dynamic
 
 const Home = () => {
     const [selected, setSelected] = useState(false);
+
+    const handleNextupPopup = () => {
+
+    }
+
+    const handleClosePopup = () => {
+
+    }
 
     return (
         <View className="w-[330px] h-[500px] mt-[120px] mx-auto">
@@ -14,14 +24,21 @@ const Home = () => {
                 <Text className="mr-2 mt-2">Time</Text>
             </View>
             <View className="justify-center items-center w-[330px] h-[100px] mb-[40px] rounded-[12px] bg-[#EFEBEB]">
-                <TouchableOpacity
+                <Habit 
+                    title="Habit"
+                    duration="Duration"
+                    handlePress={handleNextupPopup}
+                    handleClose={handleClosePopup}
+                    isNextUp={true}
+                />
+                {/* <TouchableOpacity
                     className="w-[316px] h-[75px] rounded-[12px] bg-[#D9D9D9]"
                     activeOpacity={0.7}>
                     <View className="flex-row my-auto justify-between">
                         <Text className="ml-2">Habit</Text>
                         <Text className="mr-2">Duration</Text>
                     </View>
-                </TouchableOpacity>
+                </TouchableOpacity> */}
             </View>
             <Calendar
                 onDayPress={day => {
---
+++ components/Habit.jsx
@@ -0,0 +1,43 @@
+import { TouchableOpacity, Text, View } from 'react-native'
+import React from 'react'
+
+// figure out if isLoading is needed
+
+const CloseButton = ({shouldNotRender, handlePressClose }) => {
+    if (!shouldNotRender) {
+        return (
+            <View
+                className="items-end mt-[7px] mr-[7px]"
+            >
+                <TouchableOpacity
+                    onPress={handlePressClose}
+                    className="justify-center items-center w-[8px] h-[8px] rounded-[9999px] bg-[#FF0000]"
+                >
+                    <Text
+                        className="text-[5px]"
+                    >x</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
+    return null;
+}
+
+const Habit = ({ title, duration, handlePress, handleClose, isNextUp }) => {
+    return (
+        <TouchableOpacity
+            onPress={handlePress}
+            className="w-[316px] h-[75px] rounded-[12px] bg-[#D9D9D9]"
+            activeOpacity={0.7}>
+            <CloseButton 
+                shouldNotRender={isNextUp}
+                handlePressClose={handleClose} />
+            <View className="flex-row my-auto justify-between">
+                <Text className="ml-3">{title}</Text>
+                <Text className="mr-3">{duration}</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export default Habit
