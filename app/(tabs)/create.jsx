af7c6d6179af9b15497271c97dc473d131482bc6
andrwhli <andrwhli@students.cs.ubc.ca>
2025-08-07 00:28:59 -0600

    Added start date and end date pickers

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -19,9 +19,12 @@ const Create = () => {
     const [popupVisible, setPopupVisible] = useState(false);
     const [selectedFrequency, setSelectedFrequency] = useState();
     const [time, setTime] = useState();
-    const [showPicker, setShowPicker] = useState(false);
+    const [startDate, setStartDate] = useState();
+    const [endDate, setEndDate] = useState();
+    const [showTimePicker, setShowTimePicker] = useState(false);
+    const [showStartDatePicker, setShowStartDatePicker] = useState(false);
+    const [showEndDatePicker, setShowEndDatePicker] = useState(false);
     const [form, setForm] = useState({ title: '', duration: '' });
-    const [isFocused, setIsFocused] = useState(false);
 
     const frequency = [
         { label: "Don't repeat", value: '1' },
@@ -41,18 +44,29 @@ const Create = () => {
         setHabitsArray(habitsArray => [...habitsArray, habitData]);
     };
 
+    // all these functions below are placed together for easy abstraction purposes (don't want to worry about it too much rn)
     // could want to tweak this a little bit for cancel button
-    const togglePicker = () => {
+    const toggleTimePicker = () => {
         setTime(new Date());
-        setShowPicker(!showPicker);
-    }
-    
-    const confirmIOSTime = () => {
-        setTime(time.toTimeString());
-        setShowPicker(!showPicker);
+        setShowTimePicker(!showTimePicker);
     }
 
-    const onChange = (event, selectedTime) => {
+    const confirmIOSTime = () => {
+        setTime(time.toTimeString());
+        setShowTimePicker(!showTimePicker);
+    }
+
+    const toggleStartDatePicker = () => {
+        setStartDate(new Date());
+        setShowStartDatePicker(!showStartDatePicker);
+    }
+
+    const toggleEndDatePicker = () => {
+        setEndDate(new Date());
+        setShowEndDatePicker(!showEndDatePicker);
+    }
+
+    const onTimeChange = (event, selectedTime) => {
         const currentTime = selectedTime;
         setTime(new Date(currentTime));
     }
@@ -98,7 +112,7 @@ const Create = () => {
                             <TouchableOpacity
                                 onPress={() => {
                                     setPopupVisible(false);
-                                    togglePicker();
+                                    toggleTimePicker();
                                 }}
                                 className="justify-center items-center w-[15px] h-[15px] rounded-[9999px] bg-[#FF0000] absolute"
                                 activeOpacity={0.7}
@@ -123,38 +137,36 @@ const Create = () => {
                         />
                         {/* might want to turn this into a custom component or adapt FormField with conditional rendering */}
                         <View className="space-y-2 w-80 mt-3">
-                            <View className={`justify-center border-2 border-blue-black w-full h-16 px-4 bg-black-100 rounded-2xl focus: items-center flex-row ${showPicker ? 'border-blue-500' : 'border-black-200'}`}>
+                            <View className={`justify-center border-2 border-blue-black w-full h-16 px-4 bg-black-100 rounded-2xl focus: items-center flex-row ${showTimePicker ? 'border-blue-500' : 'border-black-200'}`}>
                                 <TextInput
-                                    onFocus={() => setIsFocused(true)}
-                                    onBlur={() => setIsFocused(false)}
                                     className='flex-1 text-black font-psemibold text-base'
                                     value={time}
                                     placeholder="Time of Day"
                                     placeholderTextColor="#7b7b8b"
                                     editable={false}
-                                    onPressIn={togglePicker}
+                                    onPressIn={toggleTimePicker}
                                 />
-                                {showPicker && Platform.OS === "ios" && (
+                                {showTimePicker && Platform.OS === "ios" && (
                                     <DateTimePicker
                                         display='spinner'
                                         value={time}
                                         mode='time'
-                                        onChange={onChange}
+                                        onChange={onTimeChange}
                                         style={{
                                             maxHeight: 56,
                                             maxWidth: 280,
-                                            alignSelf: 'center',
+                                            alignItems: 'center',
                                             justifyContent: 'center'
                                         }}
                                     />
                                 )}
                             </View>
                         </View>
-                        {showPicker && Platform.OS === "ios" && (
+                        {showTimePicker && Platform.OS === "ios" && (
                             <View className="w-[280px] h-[56px] mt-3 rounded-2xl flex-row justify-around items-center">
                                 <TouchableOpacity
                                     onPress={() => {
-                                        togglePicker();
+                                        toggleTimePicker();
                                     }}
                                     className="justify-center items-center w-[88px] h-[50px] rounded-[1000px] bg-[#D9D9D9]"
                                     activeOpacity={0.7}>
@@ -174,11 +186,63 @@ const Create = () => {
                             title='Frequency'
                             items={frequency}
                         />
+                        <View className="w-[280px] h-[56px] mt-3 rounded-2xl flex-row items-center">
+                            <View className={`justify-center border-2 border-blue-black w-[140px] h-16 px-4 bg-black-100 rounded-2xl focus: items-center flex-row ${showStartDatePicker ? 'border-blue-500' : 'border-black-200'}`}>
+                                <TextInput
+                                    className='flex-1 text-black font-psemibold text-base'
+                                    value={time}
+                                    placeholder="Start Date"
+                                    placeholderTextColor="#7b7b8b"
+                                    editable={false}
+                                    onPressIn={toggleStartDatePicker}
+                                />
+                                {/* centering is only off due to the fact that it has to account for double digit days */}
+                                {showStartDatePicker && Platform.OS === "ios" && (
+                                    <DateTimePicker
+                                        display='default'
+                                        value={startDate}
+                                        mode='date'
+                                        onChange={onTimeChange}
+                                        style={{
+                                            maxHeight: 56,
+                                            maxWidth: 140,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                        }}
+                                    />
+                                )}
+                            </View>
+                            <View className={`justify-center border-2 border-blue-black w-[140px] h-16 px-4 bg-black-100 rounded-2xl focus: items-center flex-row ${showEndDatePicker ? 'border-blue-500' : 'border-black-200'}`}>
+                                <TextInput
+                                    className='flex-1 text-black font-psemibold text-base'
+                                    value={time}
+                                    placeholder="End Date"
+                                    placeholderTextColor="#7b7b8b"
+                                    editable={false}
+                                    onPressIn={toggleEndDatePicker}
+                                />
+                                {/* centering is only off due to the fact that it has to account for double digit days */}
+                                {showEndDatePicker && Platform.OS === "ios" && (
+                                    <DateTimePicker
+                                        display='default'
+                                        value={endDate}
+                                        mode='date'
+                                        onChange={onTimeChange}
+                                        style={{
+                                            maxHeight: 56,
+                                            maxWidth: 140,
+                                            alignItems: 'center',
+                                            justifyContent: 'center',
+                                        }}
+                                    />
+                                )}
+                            </View>
+                        </View>
                         <TouchableOpacity
                             onPress={() => {
                                 addHabit();
                                 setPopupVisible(false);
-                                togglePicker();
+                                toggleTimePicker();
                             }}
                             className="justify-center items-center mt-3 w-[88px] h-[50px] rounded-[1000px] bg-[#D9D9D9]"
                             activeOpacity={0.7}>
