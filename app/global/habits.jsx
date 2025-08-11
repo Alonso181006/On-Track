5dfd766697b5ba4d561ef6214978e5fd9ee11724
andrwhli <andrwhli@students.cs.ubc.ca>
2025-08-11 00:28:52 -0600

    Fixed bugs related to adding multiple habits to a day and adding habits to different days

--- app/global/habits.jsx
+++ app/global/habits.jsx
@@ -5,9 +5,17 @@ export { daysMap }
 
 // date expected format: 'yyyy-mm-dd'
 export function addHabit(date, timeOfDay, title, duration) {
-    let habitsMap = new Map();
-    habitsMap.set(timeOfDay, { habitTitle: title, habitDuration: duration })
-    daysMap.set(date, habitsMap);
+    if (daysMap.has(date)) {
+        const habitsForDay = daysMap.get(date);
+        if (!habitsForDay.has(timeOfDay)) {
+            habitsForDay.set(timeOfDay, { habitTitle: title, habitDuration: duration });
+        }
+    } else {
+        let habitsMap = new Map();
+        habitsMap.set(timeOfDay, { habitTitle: title, habitDuration: duration })
+        daysMap.set(date, habitsMap);
+    }
+
 }
 
 
